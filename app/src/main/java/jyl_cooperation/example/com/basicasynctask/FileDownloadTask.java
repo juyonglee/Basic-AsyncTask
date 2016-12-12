72c39ad36325971a912ec790ae58c857aaec185f
JuYong Lee <juyonglee0208@gmail.com>
2016-12-12 23:23:15 +0900

    File Download 기능 작성 및 Test

--- app/src/main/java/jyl_cooperation/example/com/basicasynctask/FileDownloadTask.java
+++ app/src/main/java/jyl_cooperation/example/com/basicasynctask/FileDownloadTask.java
@@ -1,33 +1,37 @@
 package jyl_cooperation.example.com.basicasynctask;
 
 import android.os.AsyncTask;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * Created by leejuyong on 2016. 12. 12..
  */
 
-public class FileDownloadTask extends AsyncTask {
+public class FileDownloadTask extends AsyncTask<String, Integer, Boolean> {
 
     private TextView downlaodStateTextView = null;
-    public FileDownloadTask(TextView downloadStateTextView) {
+    private Button downloadButton = null;
+
+    public FileDownloadTask(TextView downloadStateTextView, Button downloadButton) {
         this.downlaodStateTextView = downloadStateTextView;
+        this.downloadButton = downloadButton;
     }
 
     @Override
     protected void onPreExecute() {
         downlaodStateTextView.setText("File Downlaod....");
+        downloadButton.setText(R.string.download_button_state_cancle);
     }
 
     @Override
-    protected Object doInBackground(Object[] downloadInfos) {
+    protected Boolean doInBackground(String... downloadInfos) {
         for(int i=1; i<=downloadInfos.length; i++) {
             // 파일 내려받기 처리 상태를 표시하기 위해 호출
             publishProgress(i, downloadInfos.length);
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
                 return false;
             }
         }
@@ -35,28 +39,26 @@ public class FileDownloadTask extends AsyncTask {
     }
 
     @Override
-    protected void onProgressUpdate(Object[] values) {
-        int currentCount = (int) values[0];
-        int totalCount = (int) values[1];
+    protected void onProgressUpdate(Integer... values) {
+        int currentCount = values[0];
+        int totalCount = values[1];
         downlaodStateTextView.setText("Downloading: " + currentCount + "/" + totalCount);
     }
 
     @Override
-    protected void onPostExecute(Object o) {
+    protected void onPostExecute(Boolean o) {
         if((boolean) o == true)
         {
             downlaodStateTextView.setText("Download Finish");
         } else {
             downlaodStateTextView.setText("Download Fail");
         }
-    }
-
-    @Override
-    protected void onCancelled(Object o) {
+        downloadButton.setText(R.string.downlaod_button_state_start);
     }
 
     @Override
     protected void onCancelled() {
         downlaodStateTextView.setText("DownLoad Cancle");
+        downloadButton.setText(R.string.downlaod_button_state_start);
     }
 }
--- app/src/main/java/jyl_cooperation/example/com/basicasynctask/MainActivity.java
+++ app/src/main/java/jyl_cooperation/example/com/basicasynctask/MainActivity.java
@@ -1,17 +1,45 @@
 package jyl_cooperation.example.com.basicasynctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView downloadStateTextView = null;
     FileDownloadTask fileDownloadTask = null;
+    Button fileDownloadButton = null;
+    String[] downloadFile = {"FileUrl1", "FileUrl2", "FileUrl3", "FileUrl4", "FileUrl5", "FileUrl6", "FileUrl7", "FileUrl8", "FileUrl9", "FileUrl10"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadStateTextView = (TextView) findViewById(R.id.download_state_textview);
+        fileDownloadButton = (Button) findViewById(R.id.download_action_button);
+
+        fileDownloadTask = new FileDownloadTask(downloadStateTextView, fileDownloadButton);
+
+        fileDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(fileDownloadTask != null && fileDownloadTask.getStatus() == AsyncTask.Status.PENDING) {
+                    fileDownloadTask.execute(downloadFile);
+                }
+                else if(fileDownloadTask.getStatus() == AsyncTask.Status.RUNNING) {
+                    fileDownloadTask.cancel(true);
+                }
+                else if(fileDownloadTask.getStatus() == AsyncTask.Status.FINISHED) {
+                    if(fileDownloadTask != null) {
+                        fileDownloadTask = new FileDownloadTask(downloadStateTextView, fileDownloadButton);
+                        fileDownloadTask.execute(downloadFile);
+                    }
+                }
+            }
+        });
     }
 }
